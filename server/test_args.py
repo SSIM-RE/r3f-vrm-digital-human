4e4fb7ab298e3e3cb5f760fcf4a758ec2c1a1df0
jim <jim@example.com>
2026-03-28 14:39:15 +0800

    chore: 移除测试脚本

--- server/test_args.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.argv = ['prog', '--model_path', 'test.pt', '--prefix_motion', 'test.npy']
-from utils.parser_util import generate_args
-args = generate_args()
-print('Has prefix_motion:', hasattr(args, 'prefix_motion'))
-print('Value:', getattr(args, 'prefix_motion', 'NOT_FOUND'))
